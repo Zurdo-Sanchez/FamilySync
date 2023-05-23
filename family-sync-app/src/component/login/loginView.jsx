113198b8b2c4944237fe13b3bc21d2e51855e6ae
Juanmanuel <jumasanchez@gmail.com>
2023-05-23 12:05:55 +0200

    hola nico

--- family-sync-app/src/component/login/loginView.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-function loginView(params) {
-    
-}
-
-export default LoginView;
---
+++ family-sync-app/src/component/loginView/loginView.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function LoginView(props) {
+    
+    return (
+        <>
+            <h1>
+                Hola nico
+            </h1>
+        </>
+    )
+}
+
+export default LoginView;
