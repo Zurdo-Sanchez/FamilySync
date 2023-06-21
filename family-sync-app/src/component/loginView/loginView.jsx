700928da9b4388a144c0273703a42b3dd11b66b5
agenciamarkeen <agenciamarkeen@gmail.com>
2023-06-21 16:59:21 +0200

    Actualizacion CSS Login a MUI

--- family-sync-app/src/component/loginView/loginView.jsx
+++ family-sync-app/src/component/loginView/loginView.jsx
@@ -1,21 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Styles from "../../styles/loginStyles.js";
-
-import MailIcon from "@mui/icons-material/Mail";
-// import GoogleIcon from "@mui/icons-material/Google";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import FacebookIcon from "@mui/icons-material/Facebook";
-
-import { Button, Grid, Input } from "@mui/material";
+import { Avatar, Button, Grid, Input, Link } from "@mui/material";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
-
 import authFunction from "../../utils/authFunction";
-import GoogleIcon from "../../utils/media/google.png";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import MailLockIcon from "@mui/icons-material/MailLock";
+import GoogleIcon from "@mui/icons-material/Google";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import theme from "../../utils/Theme.js";
 
 function LoginView(props) {
   const classes = Styles();
@@ -57,102 +54,96 @@ function LoginView(props) {
   }, [getIsLoggedSelector]);
 
   return (
-    <Grid
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-start",
-        height: "100vh",
-        backgroundColor: "#c0cfc0",
-      }}
-    >
-      <Paper
-        className={classes.paperLogin}
-        style={{ width: 400, height: 450 }}
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          gap: 2,
-          overflow: "hidden",
-          mt: 5,
-        }}
-      >
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: "grid",
-            gap: 2,
+    <Grid className={classes.gridMain}>
+      <Paper elevation={10} className={classes.paperStyle}>
+        <Grid align="center">
+          <Avatar className={classes.avatarStyle}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <h2 className={classes.titleH2}>FamilySinc</h2>
+        </Grid>
+        <TextField
+          style={{ marginBottom: "10px" }}
+          required
+          fullWidth
+          type="text"
+          id="outlined-required"
+          label="UserName"
+          placeholder="Enter UserName"
+          onChange={(e) => {
+            setEmail(e.currentTarget.value);
+          }}
+        />
+        <TextField
+          style={{ marginBottom: "10px" }}
+          required
+          fullWidth
+          id="outlined-required"
+          label="Password"
+          placeholder="Enter Password"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.currentTarget.value);
+          }}
+        />
+        <Button
+          style={{ marginBottom: "10px" }}
+          className={classes.signBtnStyle}
+          variant="contained"
+          type="submit"
+          fullWidth
+        >
+          SIGN IN
+        </Button>
+        <Typography
+          style={{
+            marginBottom: "10px",
+            textAlign: "center",
+            fontWeight: "bold",
           }}
         >
-          <Typography variant="h3" className={classes.customTypography}>
-            Family Sync Login
-          </Typography>
+          <Link href="#">Forgot Password ?</Link>
+        </Typography>
+        <Typography
+          style={{
+            marginBottom: "10px",
+            textAlign: "center",
+          }}
+        >
+          {" "}
+          Do you have an account ? <Link href="#">REGISTER</Link>
+        </Typography>
 
-          <Grid className="blockLogin">
-            <Typography>Nombre</Typography>
-
-            <TextField
-              gap="4"
-              required
-              id="outlined-required"
-              label="Required"
-              type="text"
-              onChange={(e) => {
-                setEmail(e.currentTarget.value);
-              }}
-            />
-          </Grid>
-
-          <Grid>
-            <Typography>Password</Typography>
-
-            <TextField
-              required
-              id="outlined-required"
-              label="Required"
-              type="password"
-              onChange={(e) => {
-                setPassword(e.currentTarget.value);
-              }}
-            />
-          </Grid>
-
-          <Box style={{ display: "flex", justifyContent: "space-between" }}>
-            <Button variant="contained">¿Forgot your Password?</Button>
-
-            <Button variant="contained">Sign Up</Button>
-          </Box>
-
-          <Grid>
-            <Typography variant="h6">
-              I Want To <Button variant="contained">Register</Button>
-            </Typography>
-          </Grid>
-
-          <Box style={{ display: "flex", justifyContent: "space-between" }}>
-            <Grid>
-              <img
-                className={classes.googleIcon}
-                src={GoogleIcon}
-                onClick={() => handledDataLogin("email")}
-              />
-            </Grid>
-            <Grid>
-              <img
-                className={classes.googleIcon}
-                src={GoogleIcon}
-                onClick={() => handledDataLogin("google")}
-              />
-            </Grid>
-            <Grid>
-              <img
-                className={classes.googleIcon}
-                src={GoogleIcon}
-                onClick={() => handledDataLogin("google")}
-              />
-            </Grid>
-          </Box>
-        </Box>
+        <Button
+          style={{ marginBottom: "10px" }}
+          variant="outlined"
+          onClick={() => handledDataLogin("email")}
+          color="success"
+          startIcon={<MailLockIcon />}
+          fullWidth
+        >
+          Iniciar con Mail
+        </Button>
+        <Button
+          style={{ marginBottom: "10px" }}
+          variant="outlined"
+          onClick={() => handledDataLogin("google")}
+          color="success"
+          startIcon={<GoogleIcon />}
+          fullWidth
+        >
+          Iniciar con Google
+        </Button>
+        <Button
+          style={{ marginBottom: "10px" }}
+          variant="outlined"
+          onClick={() => handledDataLogin("google")}
+          color="success"
+          startIcon={<GitHubIcon />}
+          fullWidth
+        >
+          Iniciar con Git
+        </Button>
       </Paper>
     </Grid>
   );
